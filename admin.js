8777836faef4174f33d96b8b5c644e67fe3f0266
dpereirv <71473339+dpereirv@users.noreply.github.com>
2023-07-04 00:48:23 -0500

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const engine = require('ejs-locals');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+const path = require('path');
+const mongoose = require('mongoose');
+
+const db = require('./db');
+
+//const AdminBookings = require('./controllers/admin/bookings')
+const AdminClients = require('./controllers/admin/clients')
+const AdminUsers = require('./controllers/admin/users')
+
+app.use(bodyParser.json({ type: 'application/json' }))
+// Use the session middleware
+app.use(session({ 
+    secret: 'Thisisthepassword', 
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.engine('ejs', engine);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use('/users', AdminUsers)
+//app.use('/', AdminBookings);
+app.use('/', AdminClients);
+
+app.use(express.static(path.join(__dirname, "build")));
+
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
---
+++ db.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const USER = 'gordoner83';
+const PASSWORD = '7XQQ4fJlqS3dUzHt';
+const DATABASE_NAME = 'practica3';
+
+
+const URL = `mongodb+srv://dpereirv:CdxikyUjRIQSTwOM@cluster0.apapsky.mongodb.net/practica3?retryWrites=true&w=majority`;
+             
+mongoose.connect(URL)
+.then(() => {
+    console.log('Database connected!');
+})
+.catch((error)=> {
+    console.log('Error connecting:', error);
+});
+
+
+
+// const bookings = mongoose.Schema({
+//     first_name: String,
+//     last_name: String,
+//     address: String,
+//     city: String
+// });
+
+
+// const Bookings = new mongoose.model('booking', bookings);
+
+// const newbooking = new Bookings({
+//     first_name: 'Leonardo',
+//     last_name: 'Larrea',
+//     address: 'Kennedy Norte',
+//     city: 'Guayaquil'
+// })
+// .save()
+// .then((b) => {
+//     console.log('Booking:', b);
+// })
+// .catch((error) => {
+//     console.log('Error:', error);
+// })
+// Bookings.find({ first_name: 'Eduardo'})
+// .then((b) => {
+//  console.log('Booking:', b)
+// })
+// .catch((error) => {
+//  console.log('Error:', error);
+// })
+
+
+
+
